7e47aca5a7c89a89397e71a874e9190ac07efdb4
Felix Miño <felix.minom@gmail.com>
2021-08-26 23:02:49 -0500

    property testing

---
+++ property_testing/fixed_sum.py
@@ -0,0 +1,12 @@
+def sum(num1, num2):
+    """Buggy logic"""
+    if num1 == 3 and num2 == 5:
+        return 8
+    elif num1 == -2 and num2  == -2 :
+        return -4
+    elif num1 == -1 and num2 == 5 :
+        return 4
+    elif num1 == 3 and num2 == -5:
+        return -2
+    elif num1 == 0 and num2 == 5:
+        return 5
---
+++ property_testing/test_sum.py
@@ -0,0 +1,46 @@
+import pytest
+from sum import sum
+
+def test_sum():
+    assert sum(1,2) == 3
+
+# from fixed_sum import sum as fixed_sum
+
+@pytest.mark.parametrize(
+    'num1, num2, expected',
+    [(3,5,8),(-2,-2,-4), (-1,5,4), (3,-5,-2), (0,5,5)])
+def test_sum_parametized(num1, num2, expected):
+    assert sum(num1, num2) == expected
+
+from hypothesis import given, settings, Verbosity, example
+import hypothesis.strategies as st
+
+@given(st.integers(), st.integers())
+def test_sum_hypothesis(num1, num2):
+    assert sum(num1, num2) == num1 + num2
+
+@settings(verbosity=Verbosity.verbose)
+@given(st.integers(), st.integers())
+def test_sum_verbose(num1, num2):
+    assert sum(num1, num2) == num1 + num2
+
+@settings(verbosity=Verbosity.verbose)
+@given(st.integers(), st.integers())
+@example(1, 2)
+def test_sum_verbose_with_example(num1, num2):
+    assert sum(num1, num2) == num1 + num2
+
+# Testing identity
+@given(st.integers())
+def test_sum_identity(num1):
+    assert sum(num1, 0) == num1
+
+# Testing commutative
+@given(st.integers(), st.integers())
+def test_sum(num1, num2):
+    assert sum(num1, num2) == num2 + num1
+
+@given(st.integers(), st.integers())
+def test_sum_less_than_25(num1,num2):
+    assert sum(num1, num2) == num1 + num2
+    assert num1 <= 25
